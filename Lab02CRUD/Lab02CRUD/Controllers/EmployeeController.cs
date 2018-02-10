b1a5c7e73d6981d0d348041c6aec223fbc66f4e8
Tieu Nhat To <tieunhatto@gmail.com>
2018-02-10 13:24:39 +0700

    first commit

---
+++ Lab02CRUD/Lab02CRUD/Controllers/EmployeeController.cs
@@ -0,0 +1,120 @@
+﻿using Lab02CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab02CRUD.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewAll()
+        {
+            return View(GetAllEmployee());
+        }
+        IEnumerable<Employee> GetAllEmployee()
+        {
+            using (DBModel db = new DBModel())
+            {
+                return db.Employees.ToList<Employee>();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult AddOrEdit(int id=0 )
+        {
+            Employee emp = new Employee();
+            if (id != 0)
+            {
+                using (DBModel db = new DBModel())
+                {
+                    emp = db.Employees.Where(x => x.EmployeeId == id).FirstOrDefault<Employee>();
+                }
+            }
+            
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(Employee emp)
+        {
+            try
+            {
+                if (emp.ImageUpload != null)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(emp.ImageUpload.FileName);
+                    string extension = Path.GetExtension(emp.ImageUpload.FileName);
+                    fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                    emp.ImagePath = "~/AppFiles/Images/" + fileName;
+                    emp.ImageUpload.SaveAs(Path.Combine(Server.MapPath("~/AppFiles/Images/"), fileName));
+                }
+
+                using (DBModel db = new DBModel())
+                {
+                    if (emp.EmployeeId == 0) 
+                    {
+                        db.Employees.Add(emp);
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        db.Entry(emp).State = EntityState.Modified;
+                        db.SaveChanges();
+                    }
+                    
+                }
+
+                return Json(new
+                {
+                    success = true,
+                    html = GlobalClass.RenderRazorViewToString(this, "ViewAll", GetAllEmployee()),
+                    message = "Lưu nhân viên thành công."
+                }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+
+                return Json(new {success = false, message = ex.Message}, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (DBModel db = new DBModel())
+                {
+                    Employee emp = db.Employees.Where(x => x.EmployeeId == id).FirstOrDefault<Employee>();
+                    db.Employees.Remove(emp);
+                    db.SaveChanges();
+                }
+                return Json(new
+                {
+                    success = true,
+                    html = GlobalClass.RenderRazorViewToString(this, "ViewAll", GetAllEmployee()),
+                    message = "Xóa nhân viên thành công."
+                }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+
+                return Json(new { success = false, message = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult Details(string id)
+        {
+            return View();
+        }
+    }
+}
